a5e04e393c8e7db3e012caa187a82349f35c2899
Carlos <carliborre@hotmail.com>
2017-04-20 16:35:52 +0200

    Comentarios

--- pb.c
+++ pb.c
@@ -27,10 +27,7 @@ int calculate_result(int white_balls[5], int power_ball)
 	  return -1;
 	}
     }
-  if ((power_ball < 1) || (power_ball > MAX_POWER_BALL))
-    {
-      return -1;
-    }
+	// lottery ball numbers are always shown sorted
   qsort(white_balls, 5, sizeof(int), my_sort_func);
   return 0;
 }
@@ -42,34 +39,18 @@ int main(int argc, char** argv)
 	int favorite = 0;
 	for (int i=1; i<argc; i++)
 	{
-		const char* arg = argv[i];
-		if ('-' == arg[0])
-		{
-			if (0 == strcmp(arg, "-favorite"))
-			{
-				favorite = 1;
-			}
-			else
-			{
-				goto usage_error;
-			}
-		}
-		else
-		{
-			char* endptr = NULL;
-			long val = strtol(arg, &endptr, 10);
-			if (*endptr)
-			{
-				goto usage_error;
-			}
-			balls[count_balls++] = (int) val;
-		}
+		goto usage_error;
 	}
 	if (6 != count_balls)
 	{
 		goto usage_error;
 	}
+	
+	// the power ball is always the last one given
 	int power_ball = balls[5];
+
+	// calculate result can return -1 if the ball numbers
+	// are out of range
 	int result = calculate_result(balls, power_ball);
 	if (result < 0)
 	{
